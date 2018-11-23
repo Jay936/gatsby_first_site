2f0d68e7e6687014857f6d8ed751d76f287260b3
Jaydeep Bhatt <Jaydeep.bhatt@live.com>
2018-11-23 11:45:51 -0500

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const aboutpage = () => (
+    <div>
+
+    <h1>About Me</h1>
+        <p>Hi This is Jaydeep and I have build this site from a tutorial.</p>
+            
+
+   </div>
+  )
+
+
+export default  aboutpage
